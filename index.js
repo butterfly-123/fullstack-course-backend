90e58386f3117f21a9f377b1a40efc4c6548c408
Bombel <bombel@bombel.com>
2021-03-21 14:56:25 +0100

    fix

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ app.locals.engine = engine;
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://dragon-fullstack-front.herokuapp.com');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Origin", "'https://dragon-fullstack-front.herokuapp.com");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
     next();
 });
 
