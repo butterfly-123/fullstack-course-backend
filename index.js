4331970f0136c47f0951c660da8db74adfa610f3
Bombel <bombel@bombel.com>
2021-03-21 14:58:01 +0100

    fix

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.locals.engine = engine;
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "'https://dragon-fullstack-front.herokuapp.com");
+    console.log('#### HEADERS', req.headers);
+
+    res.header("Access-Control-Allow-Origin", "https://dragon-fullstack-front.herokuapp.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
