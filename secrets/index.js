731d9c44e186e01d78e9fb04d84bbcc60b282588
butterfly-123 <58802893+butterfly-123@users.noreply.github.com>
2021-04-02 07:17:58 +0200

    Update index.js

--- secrets/index.js
+++ secrets/index.js
@@ -1,3 +1,4 @@
-const APP_SECRET = 'foo123!foo123!';
+// This should be taken from env variable but for the sake of simplicity this value is set here
+const APP_SECRET = 'SUPER_RANDOM_VALUE_PROVIDED_FROM_ENV_VAR';
 
 module.exports = { APP_SECRET };
